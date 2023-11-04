ee8efb6a251b7fe63b218e396eccf6062623ee20
Anfel-l <anfellr11@gmail.com>
2023-11-04 03:36:33 -0500

    feat(DB): Location package modified and simplified

--- DB/paquetes/data_access/PCK_LOCATION.sql
+++ DB/paquetes/data_access/PCK_LOCATION.sql
@@ -1,141 +1,90 @@
 CREATE OR REPLACE PACKAGE PCK_LOCATION IS
-    /*******************************************************************************
-    Description: Package for data manipulation of the table APPOINMENT_FEE
-    Author: Andrés Felipe Lugo Rodríguez
-    Date: 17/10/2023
-    @copyright: Seguros Bolívar
-    *******************************************************************************/
-
-    /* Data types declaration */
-
-    SUBTYPE tyrcLOCATION IS LOCATION%ROWTYPE;
-    TYPE tytbLOCATION IS TABLE OF tyrcLOCATION INDEX BY BINARY_INTEGER;
-
-    /* Variables declaration */
-
-    /* Procedures and function declaration*/
-
-    /*******************************************************************************
-    Description: Procedure that inserts information in the table LOCATION
-    Author: Andrés Felipe Lugo Rodríguez
-    Date: 17/10/2023
-    @copyright: Seguros Bolívar
-    *******************************************************************************/
-    PROCEDURE Proc_Insert_LOCATION (
-        IOp_LOCATION IN OUT NOCOPY tyrcLOCATION
+    PROCEDURE Proc_Insert_LOCATION(
+        ip_location_name IN VARCHAR2
     );
 
-    /*******************************************************************************
-    Description: Procedure that gets all the information of the table LOCATION
-    Author: Andrés Felipe Lugo Rodríguez
-    Date: 17/10/2023
-    @copyright: Seguros Bolívar
-    *******************************************************************************/
-    PROCEDURE Proc_Get_All_LOCATION (
-        Op_LOCATION OUT NOCOPY tytbLOCATION
+    PROCEDURE Proc_Update_LOCATION(
+        ip_location_id IN NUMBER,
+        ip_location_name IN VARCHAR2
     );
 
-    /*******************************************************************************
-    Description: Procedure that gets information of the table LOCATION by id
-    Author: Andrés Felipe Lugo Rodríguez
-    Date: 17/10/2023
-    @copyright: Seguros Bolívar
-    *******************************************************************************/
-    PROCEDURE Proc_Get_LOCATION (
-        Ip_Id in NUMBER,
-        Op_LOCATION OUT NOCOPY tyrcLOCATION
+    PROCEDURE Proc_Get_All_LOCATION(
+        Op_location OUT SYS_REFCURSOR
     );
 
-    /*******************************************************************************
-    Description: Procedure that updates information of the table LOCATION
-    Author: Andrés Felipe Lugo Rodríguez
-    Date: 17/10/2023
-    @copyright: Seguros Bolívar
-    *******************************************************************************/
-    PROCEDURE Proc_Update_LOCATION (
-        IOp_LOCATION IN OUT NOCOPY tyrcLOCATION
+    PROCEDURE Proc_Get_LOCATION_BY_ID(
+        ip_location_id IN NUMBER,
+        Op_location OUT SYS_REFCURSOR
     );
-
 END PCK_LOCATION;
 
 CREATE OR REPLACE PACKAGE BODY PCK_LOCATION AS
+    
+    PROCEDURE Proc_Insert_LOCATION(
+        ip_location_name IN VARCHAR2
+    )IS
+    v_location_id NUMBER;
+    BEGIN
+    v_location_id := SEQ_LOCATION.NEXTVAL;
+    INSERT INTO LOCATION(
+        location_id,
+        location_name)
+    VALUES(
+        v_location_id,
+        ip_location_name);
+    EXCEPTION
+        WHEN DUP_VAL_ON_INDEX THEN
+            RAISE_APPLICATION_ERROR(-20001, 'Location already exists');
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20005, 'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);        
+    END;
 
-    /* Insert Procedure */
-    PROCEDURE Proc_Insert_LOCATION (IOp_LOCATION IN OUT NOCOPY tyrcLOCATION) IS
-        BEGIN
+    PROCEDURE Proc_Update_LOCATION(
+        ip_location_id IN NUMBER,
+        ip_location_name IN VARCHAR2
+    )IS
+    BEGIN
+        UPDATE LOCATION
+        SET
+            location_name = ip_location_name
+        WHERE
+            location_id = ip_location_id;
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20005, 'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);        
+    END;
 
-            /* Initialazing values */
-            IOp_LOCATION.location_id := SEQ_LOCATION.NEXTVAL;
+    PROCEDURE Proc_Get_All_LOCATION(
+        Op_location OUT SYS_REFCURSOR
+    )IS
+    BEGIN
+        OPEN Op_location FOR
+        SELECT
+            location_id,
+            location_name
+        FROM
+            LOCATION;
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20005, 'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);        
+    END;
 
-            /* Inserting a register */
-            INSERT INTO LOCATION VALUES /*+PCK_LOCATION*/ IOp_LOCATION;
-
-        /* Trowing Exception */
-        EXCEPTION
-            WHEN DUP_VAL_ON_INDEX THEN
-                RAISE_APPLICATION_ERROR(-20000, 'Error: Duplicated value on [PCK_LOCATION.Proc_Insert_LOCATION]');
-            WHEN OTHERS THEN
-                RAISE_APPLICATION_ERROR(-20001, SQLCODE || ' => ' || SQLERRM);
-    END Proc_Insert_LOCATION;
-
-    /* Get All Information Procedure */
-    PROCEDURE Proc_Get_All_LOCATION (Op_LOCATION OUT NOCOPY tytbLOCATION) IS
-        CURSOR cur_LOCATION IS
-            SELECT
-                location_id,
-                location_name
-            FROM LOCATION;
-                idx BINARY_INTEGER := 1;
-        BEGIN
-            FOR rec IN cur_LOCATION LOOP
-                Op_LOCATION(idx) := rec;
-                idx := idx + 1;
-            END LOOP;
-
-        EXCEPTION
-            WHEN NO_DATA_FOUND THEN 
-	            RAISE_APPLICATION_ERROR(-20150, 'Error: No results were found [PCK_LOCATION.Proc_Get_LOCATION]');
-	        WHEN OTHERS THEN
-	            RAISE_APPLICATION_ERROR(-20199, SQLCODE || ' => ' || SQLERRM);
-
-    END Proc_Get_All_LOCATION;
-
-    /* Get Information by Id Procedure */
-    PROCEDURE Proc_Get_LOCATION (Ip_Id in NUMBER, Op_LOCATION OUT NOCOPY tyrcLOCATION) IS
-        CURSOR cur_LOCATION IS
-            SELECT
-                location_id,
-                location_name
-            FROM LOCATION
-            WHERE /*+PCK_LOCATION.Proc_Get_LOCATION*/ location_id = Ip_Id;          
-        BEGIN
-            OPEN cur_LOCATION;
-            FETCH cur_LOCATION INTO Op_LOCATION;
-            CLOSE cur_LOCATION;
-
-        EXCEPTION
-            WHEN NO_DATA_FOUND THEN 
-	            RAISE_APPLICATION_ERROR(-20150, 'Error: No results were found [PCK_LOCATION.Proc_Get_LOCATION]');
-	        WHEN OTHERS THEN
-	            RAISE_APPLICATION_ERROR(-20199, SQLCODE || ' => ' || SQLERRM);
-
-    END Proc_Get_LOCATION;
-
-    /* Update Information Procedure */
-    PROCEDURE Proc_Update_LOCATION (IOp_LOCATION IN OUT NOCOPY tyrcLOCATION) IS
-        /* Declaring variable for update */
-            v_updated_record tyrcLOCATION;
-        BEGIN
-				UPDATE LOCATION
-				SET
-				    location_name = IOp_LOCATION.location_name
-				WHERE /*+PCK_LOCATION.Proc_Update_LOCATION*/ location_id = IOp_LOCATION.location_id;    
-                Proc_Get_LOCATION(IOp_LOCATION.location_id, v_updated_record);
-                IOp_LOCATION := v_updated_record;
-
-        EXCEPTION
-            WHEN OTHERS THEN
-                RAISE_APPLICATION_ERROR(-20199, SQLCODE || ' => ' || SQLERRM);
-    END Proc_Update_LOCATION;
+    PROCEDURE Proc_Get_LOCATION_BY_ID(
+        ip_location_id IN NUMBER,
+        Op_location OUT SYS_REFCURSOR
+    )IS
+    BEGIN
+        OPEN Op_location FOR
+        SELECT
+            location_id,
+            location_name
+        FROM
+            LOCATION
+        WHERE
+            location_id = ip_location_id;
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20005, 'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);        
+    END;
 
 END PCK_LOCATION;
