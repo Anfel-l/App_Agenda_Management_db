1c6e7f9e526a4ebb97ba9cbc5eae7e69cb30eba5
Anfel-l <anfellr11@gmail.com>
2023-11-04 04:38:13 -0500

    feat(DB): Contract type and appointment fee packages modified and simplified

--- DB/paquetes/data_access/PCK_APPOINTMENT_FEE.sql
+++ DB/paquetes/data_access/PCK_APPOINTMENT_FEE.sql
@@ -1,146 +1,98 @@
 CREATE OR REPLACE PACKAGE PCK_APPOINTMENT_FEE IS
-    /*******************************************************************************
-    Description: Package for data manipulation of the table APPOINMENT_FEE
-    Author: Andrés Felipe Lugo Rodríguez
-    Date: 17/10/2023
-    @copyright: Seguros Bolívar
-    *******************************************************************************/
-
-    /* Data types declaration */
-
-    SUBTYPE tyrcAPPOINTMENT_FEE IS APPOINTMENT_FEE%ROWTYPE;
-    TYPE tytbAPPOINTMENT_FEE IS TABLE OF tyrcAPPOINTMENT_FEE INDEX BY BINARY_INTEGER;
-
-    /* Variables declaration */
-
-    /* Procedures and function declaration*/
-
-    /*******************************************************************************
-    Description: Procedure that inserts information in the table APPOINTMENT_FEE
-    Author: Andrés Felipe Lugo Rodríguez
-    Date: 17/10/2023
-    @copyright: Seguros Bolívar
-    *******************************************************************************/
-    PROCEDURE Proc_Insert_APPOINTMENT_FEE (
-        IOp_Appointment_Fee IN OUT NOCOPY tyrcAPPOINTMENT_FEE
+    PROCEDURE Proc_Insert_APPOINTMENT_FEE(
+        Ip_contract_type_id IN NUMBER,
+        Ip_fee_value IN NUMBER
     );
 
-    /*******************************************************************************
-    Description: Procedure that gets all the information of the table APPOINTMENT_FEE
-    Author: Andrés Felipe Lugo Rodríguez
-    Date: 17/10/2023
-    @copyright: Seguros Bolívar
-    *******************************************************************************/
-    PROCEDURE Proc_Get_All_APPOINTMENT_FEE (
-        Op_Appointment_Fee OUT NOCOPY tytbAPPOINTMENT_FEE
+    PROCEDURE Proc_Update_APPOINTMENT_FEE(
+        Ip_appointment_fee_id IN NUMBER,
+        Ip_contract_type_id IN NUMBER,
+        Ip_fee_value IN NUMBER
     );
 
-    /*******************************************************************************
-    Description: Procedure that gets information of the table APPOINTMENT_FEE by id
-    Author: Andrés Felipe Lugo Rodríguez
-    Date: 17/10/2023
-    @copyright: Seguros Bolívar
-    *******************************************************************************/
-    PROCEDURE Proc_Get_APPOINTMENT_FEE (
-        Ip_Id in NUMBER,
-        Op_Appointment_Fee OUT NOCOPY tyrcAPPOINTMENT_FEE
+    PROCEDURE Proc_Get_All_APPOINTMENT_FEE(
+        Op_appointment_fee OUT SYS_REFCURSOR
     );
 
-    /*******************************************************************************
-    Description: Procedure that updates information of the table APPOINTMENT_FEE
-    Author: Andrés Felipe Lugo Rodríguez
-    Date: 17/10/2023
-    @copyright: Seguros Bolívar
-    *******************************************************************************/
-    PROCEDURE Proc_Update_APPOINTMENT_FEE (
-        IOp_Appointment_Fee IN OUT NOCOPY tyrcAPPOINTMENT_FEE
+    PROCEDURE Proc_Get_APPOINTMENT_FEE_BY_CONTRACT_TYPE_ID(
+        Ip_contract_type_id IN NUMBER,
+        Op_appointment_fee OUT SYS_REFCURSOR
     );
 
 END PCK_APPOINTMENT_FEE;
 
 CREATE OR REPLACE PACKAGE BODY PCK_APPOINTMENT_FEE AS
-
-    /* Insert Procedure */
-    PROCEDURE Proc_Insert_APPOINTMENT_FEE (IOp_Appointment_Fee IN OUT NOCOPY tyrcAPPOINTMENT_FEE) IS
-        BEGIN
-
-            /* Initialazing values */
-            IOp_Appointment_Fee.appointment_fee_id := SEQ_APPOINTMENT_FEE.NEXTVAL;
-
-            /* Inserting a register */
-            INSERT INTO APPOINTMENT_FEE VALUES /*+PCK_APPOINTMENT_FEE*/ IOp_Appointment_Fee;
-
-        /* Trowing Exception */
-        EXCEPTION
-            WHEN DUP_VAL_ON_INDEX THEN
-                RAISE_APPLICATION_ERROR(-20000, 'Error: Duplicated value on [PCK_APPOINTMENT_FEE.Proc_Insert_APPOINTMENT_FEE]');
-            WHEN OTHERS THEN
-                RAISE_APPLICATION_ERROR(-20001, SQLCODE || ' => ' || SQLERRM);
+    PROCEDURE Proc_Insert_APPOINTMENT_FEE(
+        Ip_contract_type_id IN NUMBER,
+        Ip_fee_value IN NUMBER
+    ) IS
+    v_appointment_fee_id NUMBER;
+    BEGIN
+    v_appointment_fee_id := SEQ_APPOINTMENT_FEE.NEXTVAL;
+        INSERT INTO APPOINTMENT_FEE
+        (
+            appointment_fee_id,
+            contract_type_id,
+            fee_value
+        )
+        VALUES
+        (
+            v_appointment_fee_id,
+            Ip_contract_type_id,
+            Ip_fee_value
+        );
+    EXCEPTION
+        WHEN DUP_VAL_ON_INDEX THEN
+            RAISE_APPLICATION_ERROR(-20001, 'APPOINTMENT_FEE already exists.');
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20002, 'An error was encountered - ' || SQLCODE || ' - ERROR - ' || SQLERRM);
     END Proc_Insert_APPOINTMENT_FEE;
 
+    PROCEDURE Proc_Update_APPOINTMENT_FEE(
+        Ip_appointment_fee_id IN NUMBER,
+        Ip_contract_type_id IN NUMBER,
+        Ip_fee_value IN NUMBER
+    ) IS
+    BEGIN
+        UPDATE APPOINTMENT_FEE
+        SET
+            contract_type_id = Ip_contract_type_id,
+            fee_value = Ip_fee_value
+        WHERE appointment_fee_id = Ip_appointment_fee_id;
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20002, 'An error was encountered - ' || SQLCODE || ' - ERROR - ' || SQLERRM);
+    END Proc_Update_APPOINTMENT_FEE;
 
-    /* Get All Information Procedure */
-    PROCEDURE Proc_Get_All_APPOINTMENT_FEE (Op_Appointment_Fee OUT NOCOPY tytbAPPOINTMENT_FEE) IS
-        CURSOR cur_APPOINTMENT_FEE IS
+    PROCEDURE Proc_Get_All_APPOINTMENT_FEE(
+        Op_appointment_fee OUT SYS_REFCURSOR
+    ) IS
+    BEGIN
+        OPEN Op_appointment_fee FOR
             SELECT
                 appointment_fee_id,
                 contract_type_id,
                 fee_value
             FROM APPOINTMENT_FEE;
-                idx BINARY_INTEGER := 1;
-        BEGIN
-            FOR rec IN cur_APPOINTMENT_FEE LOOP
-                Op_Appointment_Fee(idx) := rec;
-                idx := idx + 1;
-            END LOOP;
-
-        EXCEPTION
-            WHEN NO_DATA_FOUND THEN 
-	            RAISE_APPLICATION_ERROR(-20150, 'Error: No results were found [PCK_APPOINTMENT_FEE.Proc_Get_APPOINTMENT_FEE]');
-	        WHEN OTHERS THEN
-	            RAISE_APPLICATION_ERROR(-20199, SQLCODE || ' => ' || SQLERRM);
-
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20002, 'An error was encountered - ' || SQLCODE || ' - ERROR - ' || SQLERRM);
     END Proc_Get_All_APPOINTMENT_FEE;
 
-
-    /* Get Information by Id Procedure */
-    PROCEDURE Proc_Get_APPOINTMENT_FEE (Ip_Id in NUMBER, Op_Appointment_Fee OUT NOCOPY tyrcAPPOINTMENT_FEE) IS
-        CURSOR cur_APPOINTMENT_FEE IS
+    PROCEDURE Proc_Get_APPOINTMENT_FEE_BY_CONTRACT_TYPE_ID(
+        Ip_contract_type_id IN NUMBER,
+        Op_appointment_fee OUT SYS_REFCURSOR
+    ) IS
+    BEGIN
+        OPEN Op_appointment_fee FOR
             SELECT
                 appointment_fee_id,
                 contract_type_id,
                 fee_value
             FROM APPOINTMENT_FEE
-            WHERE /*+PCK_APPOINTMENT_FEE.Proc_Get_APPOINTMENT_FEE*/ appointment_fee_id = Ip_Id;          
-        BEGIN
-            OPEN cur_APPOINTMENT_FEE;
-            FETCH cur_APPOINTMENT_FEE INTO Op_Appointment_Fee;
-            CLOSE cur_APPOINTMENT_FEE;
-
-        EXCEPTION
-            WHEN NO_DATA_FOUND THEN 
-                RAISE_APPLICATION_ERROR(-20150, 'Error: No results were found [PCK_APPOINTMENT_FEE.Proc_Get_APPOINTMENT_FEE]');
-            WHEN OTHERS THEN
-                RAISE_APPLICATION_ERROR(-20199, SQLCODE || ' => ' || SQLERRM);
-    END Proc_Get_APPOINTMENT_FEE;
-
-
-    /* Update Information Procedure */
-    PROCEDURE Proc_Update_APPOINTMENT_FEE (IOp_Appointment_Fee IN OUT NOCOPY tyrcAPPOINTMENT_FEE) IS
-        /* Declaring variable for update */
-            v_updated_record tyrcAPPOINTMENT_FEE;
-        BEGIN
-				UPDATE APPOINTMENT_FEE
-				SET
-				    contract_type_id = IOp_Appointment_Fee.contract_type_id,
-				    fee_value = IOp_Appointment_Fee.fee_value
-				WHERE /*+PCK_APPOINTMENT_FEE.Proc_Update_APPOINTMENT_FEE*/ appointment_fee_id = IOp_Appointment_Fee.appointment_fee_id;    
-                Proc_Get_APPOINTMENT_FEE(IOp_Appointment_Fee.appointment_fee_id, v_updated_record);
-                IOp_Appointment_Fee := v_updated_record;
-
-        EXCEPTION
-            WHEN OTHERS THEN
-                RAISE_APPLICATION_ERROR(-20199, SQLCODE || ' => ' || SQLERRM);
-    END Proc_Update_APPOINTMENT_FEE;
-
+            WHERE contract_type_id = Ip_contract_type_id;
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20002, 'An error was encountered - ' || SQLCODE || ' - ERROR - ' || SQLERRM);
+    END Proc_Get_APPOINTMENT_FEE_BY_CONTRACT_TYPE_ID;
 END PCK_APPOINTMENT_FEE;
--- DB/paquetes/data_access/PCK_CONTRACT_TYPE.sql
+++ DB/paquetes/data_access/PCK_CONTRACT_TYPE.sql
@@ -1,147 +1,109 @@
 CREATE OR REPLACE PACKAGE PCK_CONTRACT_TYPE IS
-    /*******************************************************************************
-    Description: Package for data manipulation of the table CONTRACT_TYPE
-    Author: Andrés Felipe Lugo Rodríguez
-    Date: 17/10/2023
-    @copyright: Seguros Bolívar
-    *******************************************************************************/
-
-    /* Data types declaration */
-
-    SUBTYPE tyrcCONTRACT_TYPE IS CONTRACT_TYPE%ROWTYPE;
-    TYPE tytbCONTRACT_TYPE IS TABLE OF tyrcCONTRACT_TYPE INDEX BY BINARY_INTEGER;
-
-    /* Variables declaration */
-
-    /* Procedures and function declaration*/
-
-    /*******************************************************************************
-    Description: Procedure that inserts information in the table CONTRACT_TYPE
-    Author: Andrés Felipe Lugo Rodríguez
-    Date: 17/10/2023
-    @copyright: Seguros Bolívar
-    *******************************************************************************/
-    PROCEDURE Proc_Insert_CONTRACT_TYPE (
-        IOp_CONTRACT_TYPE IN OUT NOCOPY tyrcCONTRACT_TYPE
+    PROCEDURE Proc_Insert_CONTRACT_TYPE(
+        Ip_contract_type_name IN VARCHAR2,
+        Ip_description IN VARCHAR2,
+        Ip_contract_type_priority IN NUMBER
     );
 
-    /*******************************************************************************
-    Description: Procedure that gets all the information of the table CONTRACT_TYPE
-    Author: Andrés Felipe Lugo Rodríguez
-    Date: 17/10/2023
-    @copyright: Seguros Bolívar
-    *******************************************************************************/
-    PROCEDURE Proc_Get_All_CONTRACT_TYPE (
-        Op_CONTRACT_TYPE OUT NOCOPY tytbCONTRACT_TYPE
+    PROCEDURE Proc_Update_CONTRACT_TYPE(
+        Ip_contract_type_id IN NUMBER,
+        Ip_contract_type_name IN VARCHAR2,
+        Ip_description IN VARCHAR2,
+        Ip_contract_type_priority IN NUMBER
+
     );
 
-    /*******************************************************************************
-    Description: Procedure that gets information of the table CONTRACT_TYPE by id
-    Author: Andrés Felipe Lugo Rodríguez
-    Date: 17/10/2023
-    @copyright: Seguros Bolívar
-    *******************************************************************************/
-    PROCEDURE Proc_Get_CONTRACT_TYPE (
-        Ip_Id in NUMBER,
-        Op_CONTRACT_TYPE OUT NOCOPY tyrcCONTRACT_TYPE
+    PROCEDURE Proc_Get_All_CONTRACT_TYPE(
+        Op_contract_type OUT SYS_REFCURSOR
     );
 
-    /*******************************************************************************
-    Description: Procedure that updates information of the table CONTRACT_TYPE
-    Author: Andrés Felipe Lugo Rodríguez
-    Date: 17/10/2023
-    @copyright: Seguros Bolívar
-    *******************************************************************************/
-    PROCEDURE Proc_Update_CONTRACT_TYPE (
-        IOp_CONTRACT_TYPE IN OUT NOCOPY tyrcCONTRACT_TYPE
+    PROCEDURE Proc_Get_CONTRACT_TYPE_BY_ID(
+        Ip_contract_type_id IN NUMBER,
+        Op_contract_type OUT SYS_REFCURSOR
     );
 
 END PCK_CONTRACT_TYPE;
 
 CREATE OR REPLACE PACKAGE BODY PCK_CONTRACT_TYPE AS
-
-    /* Insert Procedure */
-    PROCEDURE Proc_Insert_CONTRACT_TYPE (IOp_CONTRACT_TYPE IN OUT NOCOPY tyrcCONTRACT_TYPE) IS
-        BEGIN
-
-            /* Initialazing values */
-            IOp_CONTRACT_TYPE.contract_type_id := SEQ_CONTRACT_TYPE.NEXTVAL;
-
-            /* Inserting a register */
-            INSERT INTO CONTRACT_TYPE VALUES /*+PCK_CONTRACT_TYPE*/ IOp_CONTRACT_TYPE;
-
-        /* Trowing Exception */
-        EXCEPTION
-            WHEN DUP_VAL_ON_INDEX THEN
-                RAISE_APPLICATION_ERROR(-20000, 'Error: Duplicated value on [PCK_CONTRACT_TYPE.Proc_Insert_CONTRACT_TYPE]');
-            WHEN OTHERS THEN
-                RAISE_APPLICATION_ERROR(-20001, SQLCODE || ' => ' || SQLERRM);
+    PROCEDURE Proc_Insert_CONTRACT_TYPE(
+        Ip_contract_type_name IN VARCHAR2,
+        Ip_description IN VARCHAR2,
+        Ip_contract_type_priority IN NUMBER
+    )IS
+    v_contract_type_id NUMBER;
+    BEGIN
+    v_contract_type_id := SEQ_CONTRACT_TYPE.NEXTVAL;
+    INSERT INTO CONTRACT_TYPE(
+        contract_type_id,
+        contract_type_name,
+        description,
+        contract_type_priority)
+    VALUES(
+        v_contract_type_id,
+        Ip_contract_type_name,
+        Ip_description,
+        Ip_contract_type_priority);
+    EXCEPTION
+        WHEN DUP_VAL_ON_INDEX THEN
+            RAISE_APPLICATION_ERROR(-20001, 'Duplicate value');
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20002, 'An error was encountered -' || SQLCODE || '- ERROR -' || SQLERRM);
     END Proc_Insert_CONTRACT_TYPE;
 
-    /* Get All Information Procedure */
-    PROCEDURE Proc_Get_All_CONTRACT_TYPE (Op_CONTRACT_TYPE OUT NOCOPY tytbCONTRACT_TYPE) IS
-        CURSOR cur_CONTRACT_TYPE IS
-            SELECT
-                contract_type_id,
-                contract_type_name,
-                description,
-                contract_type_priority
-            FROM CONTRACT_TYPE;
-                idx BINARY_INTEGER := 1;
-        BEGIN
-            FOR rec IN cur_CONTRACT_TYPE LOOP
-                Op_CONTRACT_TYPE(idx) := rec;
-                idx := idx + 1;
-            END LOOP;
+    PROCEDURE Proc_Update_CONTRACT_TYPE(
+        Ip_contract_type_id IN NUMBER,
+        Ip_contract_type_name IN VARCHAR2,
+        Ip_description IN VARCHAR2,
+        Ip_contract_type_priority IN NUMBER
 
-        EXCEPTION
-            WHEN NO_DATA_FOUND THEN 
-	            RAISE_APPLICATION_ERROR(-20150, 'Error: No results were found [PCK_CONTRACT_TYPE.Proc_Get_CONTRACT_TYPE]');
-	        WHEN OTHERS THEN
-	            RAISE_APPLICATION_ERROR(-20199, SQLCODE || ' => ' || SQLERRM);
-
-    END Proc_Get_All_CONTRACT_TYPE;
-
-    /* Get Information by Id Procedure */
-    PROCEDURE Proc_Get_CONTRACT_TYPE (Ip_Id in NUMBER, Op_CONTRACT_TYPE OUT NOCOPY tyrcCONTRACT_TYPE) IS
-        CURSOR cur_CONTRACT_TYPE IS
-            SELECT
-                contract_type_id,
-                contract_type_name,
-                description,
-                contract_type_priority
-            FROM CONTRACT_TYPE
-            WHERE /*+PCK_CONTRACT_TYPE.Proc_Get_CONTRACT_TYPE*/ contract_type_id = Ip_Id;          
-        BEGIN
-            OPEN cur_CONTRACT_TYPE;
-            FETCH cur_CONTRACT_TYPE INTO Op_CONTRACT_TYPE;
-            CLOSE cur_CONTRACT_TYPE;
-
-        EXCEPTION
-            WHEN NO_DATA_FOUND THEN 
-	            RAISE_APPLICATION_ERROR(-20150, 'Error: No results were found [PCK_CONTRACT_TYPE.Proc_Get_CONTRACT_TYPE]');
-	        WHEN OTHERS THEN
-	            RAISE_APPLICATION_ERROR(-20199, SQLCODE || ' => ' || SQLERRM);
-
-    END Proc_Get_CONTRACT_TYPE;
-
-    /* Update Information Procedure */
-    PROCEDURE Proc_Update_CONTRACT_TYPE (IOp_CONTRACT_TYPE IN OUT NOCOPY tyrcCONTRACT_TYPE) IS
-        /* Declaring variable for update */
-            v_updated_record tyrcCONTRACT_TYPE;
-        BEGIN
-				UPDATE CONTRACT_TYPE
-				SET
-				    contract_type_name = IOp_CONTRACT_TYPE.contract_type_name,
-				    description = IOp_CONTRACT_TYPE.description,
-                    contract_type_priority = IOp_CONTRACT_TYPE.contract_type_priority
-				WHERE /*+PCK_CONTRACT_TYPE.Proc_Update_CONTRACT_TYPE*/ contract_type_id = IOp_CONTRACT_TYPE.contract_type_id;    
-                Proc_Get_CONTRACT_TYPE(IOp_CONTRACT_TYPE.contract_type_id, v_updated_record);
-                IOp_CONTRACT_TYPE := v_updated_record;
-
-        EXCEPTION
-            WHEN OTHERS THEN
-                RAISE_APPLICATION_ERROR(-20199, SQLCODE || ' => ' || SQLERRM);
+    )IS
+    BEGIN
+        UPDATE CONTRACT_TYPE
+        SET
+            contract_type_name = Ip_contract_type_name,
+            description = Ip_description,
+            contract_type_priority = Ip_contract_type_priority
+        WHERE
+            contract_type_id = Ip_contract_type_id; 
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20002, 'An error was encountered -' || SQLCODE || '- ERROR -' || SQLERRM);
     END Proc_Update_CONTRACT_TYPE;
 
+    PROCEDURE Proc_Get_All_CONTRACT_TYPE(
+        Op_contract_type OUT SYS_REFCURSOR
+    )IS
+    BEGIN
+        OPEN Op_contract_type FOR
+            SELECT
+                contract_type_id,
+                contract_type_name,
+                description,
+                contract_type_priority
+            FROM
+                CONTRACT_TYPE;
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20002, 'An error was encountered -' || SQLCODE || '- ERROR -' || SQLERRM);
+    END Proc_Get_All_CONTRACT_TYPE;
+
+    PROCEDURE Proc_Get_CONTRACT_TYPE_BY_ID(
+        Ip_contract_type_id IN NUMBER,
+        Op_contract_type OUT SYS_REFCURSOR
+    )IS
+    BEGIN
+        OPEN Op_contract_type FOR
+            SELECT
+                contract_type_id,
+                contract_type_name,
+                description,
+                contract_type_priority
+            FROM
+                CONTRACT_TYPE
+            WHERE
+                contract_type_id = Ip_contract_type_id;
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20002, 'An error was encountered -' || SQLCODE || '- ERROR -' || SQLERRM);
+    END Proc_Get_CONTRACT_TYPE_BY_ID;
 END PCK_CONTRACT_TYPE;
