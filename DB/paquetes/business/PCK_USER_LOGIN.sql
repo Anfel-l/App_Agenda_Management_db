d332fc81b9a3e1c1733106273127418864d39b31
Anfel-l <anfellr11@gmail.com>
2023-11-05 17:45:51 -0500

    fix (DB): User Login package was succesfully managed

--- DB/paquetes/business/PCK_USER_LOGIN.sql
+++ DB/paquetes/business/PCK_USER_LOGIN.sql
@@ -15,20 +15,22 @@ CREATE OR REPLACE PACKAGE BODY PCK_USER_LOGIN IS
         Ip_Password IN VARCHAR2, 
         Op_UserId OUT NUMBER
     )IS
-        v_user_record SYS_REFCURSOR;
+        v_cursor SYS_REFCURSOR;
+        v_user_record MEDICAL_USER%ROWTYPE;    
     BEGIN
-        PCK_MEDICAL_USER.Proc_Get_MEDICAL_USER_DOCUMENT(Ip_Document, v_user_record);
-        IF v_user_record.password = Ip_Password THEN
-            Op_UserId := v_user_record.user_id;
-        ELSE
-            Op_UserId := NULL;
-        END IF;
+        PCK_MEDICAL_USER.Proc_Get_MEDICAL_USER_BY_DOCUMENT(Ip_Document, v_cursor);
+
+        FETCH v_cursor INTO v_user_record;
+            IF v_user_record.password = Ip_Password THEN
+                Op_UserId := v_user_record.user_id;
+            ELSE
+                Op_UserId := NULL;
+            END IF;
+        CLOSE v_cursor;
     EXCEPTION
         WHEN NO_DATA_FOUND THEN
-            Op_Result := 'User not found';
             Op_UserId := NULL;
         WHEN OTHERS THEN
-            Op_Result := 'Error during login';
             Op_UserId := NULL;
     END Proc_User_Login;
 END PCK_USER_LOGIN;
