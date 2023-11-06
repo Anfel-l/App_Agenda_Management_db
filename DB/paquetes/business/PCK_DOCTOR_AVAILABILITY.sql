e6b6939e73f1c1aa5e8b33f07746b9f2626ad316
Anfel-l <anfellr11@gmail.com>
2023-11-06 00:08:16 -0500

    feat(db): Doctor availability package was modified and simplified

--- DB/paquetes/business/PCK_DOCTOR_AVAILABILITY.sql
+++ DB/paquetes/business/PCK_DOCTOR_AVAILABILITY.sql
@@ -16,10 +16,25 @@ CREATE OR REPLACE PACKAGE BODY PCK_DOCTOR_AVAILABILITY AS
         Op_DOCTORS OUT SYS_REFCURSOR
     ) IS
         v_medical_field_id NUMBER;
-        v_medical_center_ids SYS_REFCURSOR;
-        idx BINARY_INTEGER := 1;
-        
-        CURSOR cur_DOCTORS(v_field_id NUMBER) IS
+        v_medical_center_id_table t_medical_center_id_table := t_medical_center_id_table();
+        v_medical_center_id NUMBER;
+        v_temp_cursor SYS_REFCURSOR;
+    BEGIN
+        SELECT medical_field_id INTO v_medical_field_id 
+        FROM MEDICAL_APPOINTMENT 
+        WHERE medical_appointment_id = Ip_medical_appointment_id;
+
+        PCK_USER_LOCATION.Proc_Get_Centers_By_User_Location(Ip_user_id, v_temp_cursor);
+
+        LOOP
+            FETCH v_temp_cursor INTO v_medical_center_id;
+            EXIT WHEN v_temp_cursor%NOTFOUND;
+            v_medical_center_id_table.EXTEND;
+            v_medical_center_id_table(v_medical_center_id_table.LAST) := v_medical_center_id;
+        END LOOP;
+        CLOSE v_temp_cursor;
+
+        OPEN Op_DOCTORS FOR
             SELECT
                 doctor_id,
                 first_name,
@@ -27,32 +42,13 @@ CREATE OR REPLACE PACKAGE BODY PCK_DOCTOR_AVAILABILITY AS
                 last_name,
                 medical_field_id,
                 medical_center_id
-            FROM DOCTOR  
-            WHERE medical_field_id = v_field_id 
-            AND medical_center_id IN (SELECT medical_center_id FROM TABLE(v_medical_center_ids));
-
-    BEGIN
-        SELECT medical_field_id INTO v_medical_field_id 
-        FROM MEDICAL_APPOINTMENT 
-        WHERE medical_appointment_id = Ip_medical_appointment_id;
-
-        PCK_USER_LOCATION.Proc_Get_Centers_By_User_Location(Ip_user_id, v_medical_center_ids);
-        
-        FOR rec IN cur_DOCTORS(v_medical_field_id) LOOP
-            Op_DOCTORS(idx).doctor_id := rec.doctor_id;
-            Op_DOCTORS(idx).first_name := rec.first_name;
-            Op_DOCTORS(idx).second_name := rec.second_name;
-            Op_DOCTORS(idx).last_name := rec.last_name;
-            Op_DOCTORS(idx).medical_field_id := rec.medical_field_id;
-            Op_DOCTORS(idx).medical_center_id := rec.medical_center_id;
-            idx := idx + 1;
-        END LOOP;
+            FROM DOCTOR
+            WHERE medical_field_id = v_medical_field_id
+            AND medical_center_id MEMBER OF v_medical_center_id_table;
 
     EXCEPTION
-        WHEN NO_DATA_FOUND THEN 
-            RAISE_APPLICATION_ERROR(-20150, 'Error: No results were found [PCK_DOCTOR_AVAILABILITY.Proc_Get_Doctors_By_Appointment]');
         WHEN OTHERS THEN
-            RAISE_APPLICATION_ERROR(-20199, SQLCODE || ' => ' || SQLERRM);
+            RAISE_APPLICATION_ERROR(-20199, 'Unexpected error: ' || SQLERRM);
     END Proc_Get_Doctors_By_Appointment;
     
 END PCK_DOCTOR_AVAILABILITY;
---
+++ DB/tipos/MED_NUMBER_TABLE_TYPE.sql
@@ -0,0 +1,1 @@
+CREATE OR REPLACE TYPE t_medical_center_id_table AS TABLE OF NUMBER;
