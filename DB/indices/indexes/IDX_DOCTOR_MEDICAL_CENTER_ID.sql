59e008ff8bc6cdda467e297989903fae5be821ab
Anfel-l <anfellr11@gmail.com>
2023-11-13 23:00:45 -0500

    fix(db): All objects of the db now commented and documented

--- DB/indices/indexes/IDX_DOCTOR_MEDICAL_CENTER_ID.sql
+++ DB/indices/indexes/IDX_DOCTOR_MEDICAL_CENTER_ID.sql
@@ -1,5 +1,5 @@
 /*******************************************************************************
-Description: Creation script for the table SYMPTOM
+Description: Creation script for the index idx_doctor_medical_center_id on the table MED_USER_DBA.DOCTOR
 Author: Andrés Felipe Lugo Rodríguez
 Date: 17/10/2023
 @copyright: Seguros Bolívar
